2ab97dc813021cdefbad5b33d02083f386cbc963
Thanathat-lert <ake12345678910@gmail.com>
2025-09-19 22:06:13 +0700

    Add edit and delete button
    - Add button after list of to do

--- frontend/src/assets/components/todo/todoList.tsx
+++ frontend/src/assets/components/todo/todoList.tsx
@@ -7,9 +7,19 @@ export default function TodoList({
   todos: Todo[];
   status: TodoStatus;
 }) {
+  function redirectToEditPage(id: number) {
+    window.location.href = `/edit/${id}`;
+  }
+
+  function deleteTodo(id: number) {
+    console.log("Delete todo with id:", id);
+  }
+
   const displayTodos = todos.map((todo, index) => (
     <li key={index}>
       {todo.task} - {todo.status} - {todo.dueDate}
+      <button onClick={() => redirectToEditPage(todo.id)}>Edit</button>
+      <button onClick={() => deleteTodo(todo.id)}>Delete</button>
     </li>
   ));
   return (
