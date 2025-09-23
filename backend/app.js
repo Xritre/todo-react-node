e534ec13778c5a8da22e8dc7e06306c5147f4382
Thanathat-lert <ake12345678910@gmail.com>
2025-09-23 22:28:44 +0700

    Connect mongoose
    - Install mongoose (connect mongoDB), multer (Parse form data)
    
    backend\app.js
    - Import used package
    - Update CORS to 5173
    - Connect MongoDB with mongoose

--- backend/app.js
+++ backend/app.js
@@ -1,10 +1,16 @@
 // Import packages
+require("dotenv").config();
+
 var createError = require("http-errors");
 var express = require("express");
 var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
 var cors = require("cors");
+var mongoose = require("mongoose");
+
+const MONGODB_URL = process.env.MONGODB_URL;
+
 // Import routes
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
@@ -13,7 +19,7 @@ var todosRouter = require("./routes/todos");
 var app = express();
 
 var corsOption = {
-  origin: ["http://localhost:5174"],
+  origin: ["http://localhost:5173"],
 };
 
 // view engine setup
@@ -21,8 +27,11 @@ app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "jade");
 
 app.use(logger("dev"));
+// Parse data in JSON and URL-encoded format
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
+// Parse formData
+
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 app.use(cors(corsOption));
@@ -31,6 +40,16 @@ app.use(cors(corsOption));
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
 app.use("/todos", todosRouter);
+
+// Connect to mongoDB
+mongoose
+  .connect(`${MONGODB_URL}/mydb`)
+  .then((value) => {
+    console.log("Connect MongoDB Succuessfully");
+  })
+  .catch((error) => {
+    console.error(`Failed to connect MongoDB due to : ${error}`);
+  });
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
