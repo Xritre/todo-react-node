372f8f13270407c0c9f4245881fa70ceea1dbf43
Thanathat-lert <ake12345678910@gmail.com>
2025-09-25 23:06:34 +0700

    Add update, delete todos method
    - Add toObject() when return todo data for replace _id with id
    - Add route for update and delete todo

--- backend/routes/todos.js
+++ backend/routes/todos.js
@@ -2,12 +2,13 @@ var express = require("express");
 var router = express.Router();
 const multer = require("multer");
 const upload = multer(); // for parsing multipart/form-data
-var { TodoModel } = require("../model/todo");
+var { TodoModel } = require("../model/Todo");
 
 router.get("/", async function (req, res) {
   const todos = await TodoModel.find();
   console.log("todos", todos);
-  res.send({ result: todos });
+  const plainTodos = todos.map((todo) => todo.toObject());
+  res.send({ result: plainTodos });
 });
 
 router.post("/", upload.none(), async function (req, res, next) {
@@ -22,6 +23,50 @@ router.post("/", upload.none(), async function (req, res, next) {
     console.log("mock todo model from backend", todo);
     await todo.save();
     res.send({ result: "Create todo success" });
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.put("/", upload.none(), async function (req, res, next) {
+  try {
+    const body = req.body;
+    const id = body.id;
+    const filter = { _id: id };
+    const replaceTodo = await TodoModel.findByIdAndUpdate(
+      filter,
+      {
+        task: body.task,
+        status: body.status,
+        dueDate: body.dueDate,
+      },
+      {
+        new: true,
+        runValidators: true,
+      }
+    );
+    if (!replaceTodo) {
+      return res.status(404).send({ message: `Todo with id ${id} not found` });
+    }
+    res.send({
+      message: "Successfully updated",
+      result: replaceTodo.toObject(),
+    });
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.delete("/:id", async function (req, res, next) {
+  try {
+    const params = req.params;
+    const id = params.id;
+    console.log("id to delete from params", id);
+    const deleteTodo = await TodoModel.findByIdAndDelete(id);
+    if (!deleteTodo) {
+      return res.status(404).send({ message: `Todo with ${id} not found` });
+    }
+    res.send({ message: "Successfully deleted" });
   } catch (error) {
     next(error);
   }
