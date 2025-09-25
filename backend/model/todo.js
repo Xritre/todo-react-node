94f473b10310af0bec73b270cdf22fc256e2c62f
Thanathat-lert <ake12345678910@gmail.com>
2025-09-25 23:04:45 +0700

    Update todo schema to return id
    - When called toObject() generate id field(virtual : true) and removed _id, __v

--- backend/model/todo.js
+++ backend/model/todo.js
@@ -1,10 +1,18 @@
 const mongoose = require("mongoose");
 
 const todoSchema = new mongoose.Schema({
-  id: String,
   task: { type: String, required: true },
   status: { type: String, required: true },
   dueDate: { type: Date, required: true },
+});
+
+todoSchema.set("toObject", {
+  virtuals: true,
+  transform: (doc, ret) => {
+    delete ret._id;
+    delete ret.__v;
+    return ret;
+  },
 });
 
 const TodoModel = new mongoose.model("Todo", todoSchema);
